1ae9cb8ca461fef35426d70720985082fb5468aa
Kanish <kanishv1012@gmail.com>
2025-01-14 09:56:15 +0530

    Created notification button and profile img

--- frontend/src/components/navbar.component.jsx
+++ frontend/src/components/navbar.component.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import logo from "../imgs/logo.png";
 import { Link, Outlet } from "react-router-dom";
+import { UserContext } from "../App";
 
 const Navbar = () => {
     const [searchBoxVisibility, setSearchBoxVisibility] = useState(false);
+
+    const {
+        userAuth,
+        userAuth: { access_token, profile_img },
+    } = useContext(UserContext);
 
     return (
         <>
@@ -48,18 +54,41 @@ const Navbar = () => {
                         <p>Write</p>
                     </Link>
 
-                    {/* sign-in button */}
-                    <Link to="/signin" className="btn-dark py-2">
-                        Sign-in
-                    </Link>
+                    {access_token ? (
+                        <>
+                            {/* Notification button */}
+                            <Link to="/dashboard/notification">
+                                <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
+                                    <i className="fi fi-rr-bell text-2xl block mt-1"></i>
+                                </button>
+                            </Link>
 
-                    {/* sign-up button */}
-                    <Link
-                        to="/signup"
-                        className="btn-light py-2 hidden md:block"
-                    >
-                        Sign-up
-                    </Link>
+                            {/* Profile img */}
+                            <div className="relative">
+                                <button className="w-12 h-12 mt-1">
+                                    <img
+                                        src={profile_img}
+                                        className="w-full h-full object-cover rounded-full"
+                                    />
+                                </button>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            {/* sign-in button */}
+                            <Link to="/signin" className="btn-dark py-2">
+                                Sign-in
+                            </Link>
+
+                            {/* sign-up button */}
+                            <Link
+                                to="/signup"
+                                className="btn-light py-2 hidden md:block"
+                            >
+                                Sign-up
+                            </Link>
+                        </>
+                    )}
                 </div>
             </nav>
 
