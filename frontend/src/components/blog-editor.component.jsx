e0ced3cd40d7a98d4d58ae010cb5af05b59caa08
Kanish <kanishv1012@gmail.com>
2025-01-15 10:57:51 +0530

    Editor page component

---
+++ frontend/src/components/blog-editor.component.jsx
@@ -0,0 +1,7 @@
+const BlogEditor = ()=>{
+    return(
+        <h1>Blog from component</h1>
+    )
+}
+
+export default BlogEditor;
---
+++ frontend/src/components/publish-form.component.jsx
@@ -0,0 +1,7 @@
+const PublishForm = ()=>{
+    return(
+        <h1>Publish form from component</h1>
+    )
+}
+
+export default PublishForm;
