2de0437db3ace6804ffda644767db846925bc377
Kanish <kanishv1012@gmail.com>
2025-01-20 17:11:00 +0530

    added close button

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -25,7 +25,7 @@ const BlogEditor = () => {
         setTextEditor(
             new EditorJS({
                 holder: "textEditor",
-                data: "",
+                data: content,
                 tools: tools,
                 placeholder: "Let's write an awesome story",
             })
@@ -147,6 +147,7 @@ const BlogEditor = () => {
 
                         {/* Title input area */}
                         <textarea
+                            defaultValue={title}
                             placeholder="Blog Title"
                             className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
                             onKeyDown={handleTitleKeyDown}
--- frontend/src/components/publish-form.component.jsx
+++ frontend/src/components/publish-form.component.jsx
@@ -1,7 +1,37 @@
-const PublishForm = ()=>{
-    return(
-        <h1>Publish form from component</h1>
-    )
-}
+import { useContext } from "react";
+import AnimationWrapper from "../common/page-animation";
+import { Toaster } from "react-hot-toast";
+import { EditorContext } from "../pages/editor.pages";
+
+const PublishForm = () => {
+    let {
+        blog: { banner, title, tags, desc },
+        setEditorState,
+    } = useContext(EditorContext);
+
+    const handleCloseEvent = () => {
+        setEditorState("editor");
+    };
+    return (
+        <AnimationWrapper>
+            <section>
+                <Toaster />
+                <button
+                    className="w-12 h-12 absolute right-[5vw] z-10 top-[5%] lg:top-[10%]"
+                    onClick={handleCloseEvent}
+                >
+                    <i className="fi fi-br-cross"></i>
+                </button>
+
+                <div className="max-w-[550] center">
+                    <p className="text-dark-grey mb-1">Preview</p>
+                    <div className="w-full aspect-video rounded-lg overflow-hidden bg-grey mt-4">
+                        <img src={banner} />
+                    </div>
+                </div>
+            </section>
+        </AnimationWrapper>
+    );
+};
 
 export default PublishForm;
