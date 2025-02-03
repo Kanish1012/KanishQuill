6bffe917e97bac00adb99b4283b3b7828200921f
Kanish <kanishv1012@gmail.com>
2025-02-03 21:28:35 +0530

    added title, des

--- frontend/src/components/publish-form.component.jsx
+++ frontend/src/components/publish-form.component.jsx
@@ -4,17 +4,37 @@ import { Toaster } from "react-hot-toast";
 import { EditorContext } from "../pages/editor.pages";
 
 const PublishForm = () => {
+    let charcterLimit = 200;
     let {
-        blog: { banner, title, tags, desc },
+        blog,
+        blog: { banner, title, tags, des },
         setEditorState,
+        setBlog,
     } = useContext(EditorContext);
 
     const handleCloseEvent = () => {
         setEditorState("editor");
     };
+
+    const handleBlogTitleChange = (e) => {
+        let input = e.target;
+        setBlog({ ...blog, title: input.value });
+    };
+
+    const handleBlogDesChange = (e) => {
+        let input = e.target;
+        setBlog({ ...blog, des: input.value });
+    };
+
+    const handleTitleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+        }
+    };
+
     return (
         <AnimationWrapper>
-            <section>
+            <section className="w-screen min-h-screen grid items-center lg:grid-cols-2 py-16 lg:gap-4">
                 <Toaster />
                 <button
                     className="w-12 h-12 absolute right-[5vw] z-10 top-[5%] lg:top-[10%]"
@@ -28,6 +48,39 @@ const PublishForm = () => {
                     <div className="w-full aspect-video rounded-lg overflow-hidden bg-grey mt-4">
                         <img src={banner} />
                     </div>
+
+                    <h1 className="text-4xl font-medium mt-2 leading-tight line-clamp-2">
+                        {title}
+                    </h1>
+                    <p className="font-gelasio line-clamp-2 text-xl leading-7 mt-4">
+                        {des}
+                    </p>
+                </div>
+
+                <div className="border-grey lg:border-1 lg:pl-8">
+                    <p className="text-dark-grey mb-2 mt-9">Blog Title</p>
+                    <input
+                        type="text"
+                        placeholder="Blog Title"
+                        defaultValue={title}
+                        className="input-box pl-4"
+                        onChange={handleBlogTitleChange}
+                    />
+
+                    <p className="text-dark-grey mb-2 mt-9">
+                        Short description about your blog
+                    </p>
+                    <textarea
+                        maxLength={charcterLimit}
+                        defaultValue={des}
+                        className="h-40 resize-none leading-7 input-box pl-4"
+                        onChange={handleBlogDesChange}
+                        onKeyDown={handleTitleKeyDown}
+                    ></textarea>
+
+                    <p className="mt-1 text-dark-grey text-sm text-right">
+                        {charcterLimit - des.length} characters left
+                    </p>
                 </div>
             </section>
         </AnimationWrapper>
