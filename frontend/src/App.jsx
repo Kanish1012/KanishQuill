ffaa2fe1f81a60ef884122e5749e22158632c4bb
Kanish <kanishv1012@gmail.com>
2025-02-10 23:11:49 +0530

    setting up Home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import UserAuthForm from "./pages/userAuthForm.page";
 import { createContext, useEffect, useState } from "react";
 import { lookInSession } from "./common/session";
 import Editor from "./pages/editor.pages";
+import HomePage from "./pages/home.page";
 
 export const UserContext = createContext({});
 
@@ -36,16 +37,18 @@ const App = () => {
     return (
         <UserContext.Provider value={{ userAuth, setUserAuth }}>
             <Routes>
-                <Route path="/" element={<Navbar />} />
                 <Route path="/editor" element={<Editor />} />
-                <Route
-                    path="/signin"
-                    element={<UserAuthForm type="sign-in" />}
-                />
-                <Route
-                    path="/signup"
-                    element={<UserAuthForm type="sign-up" />}
-                />
+                <Route path="/" element={<Navbar />}>
+                    <Route index element={<HomePage />} />
+                    <Route
+                        path="/signin"
+                        element={<UserAuthForm type="sign-in" />}
+                    />
+                    <Route
+                        path="/signup"
+                        element={<UserAuthForm type="sign-up" />}
+                    />
+                </Route>
             </Routes>
         </UserContext.Provider>
     );
