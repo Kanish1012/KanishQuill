3d88e7917419435971fcee713bb72be7e3e27fe5
Kanish <kanishv1012@gmail.com>
2025-02-11 11:25:29 +0530

    fetching in frontend

--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -1,14 +1,43 @@
+import axios from "axios";
 import AnimationWrapper from "../common/page-animation";
 import InPageNavigation from "../components/inpage-navigation.component";
+import { useEffect, useState } from "react";
+import Loader from "../components/loader.component"
 
 const HomePage = () => {
+    let [blogs, setBlogs] = useState(null);
+
+    const fetchLatestBlogs = () => {
+        axios
+            .get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
+            .then(({ data }) => {
+                setBlogs(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    useEffect(() => {
+        fetchLatestBlogs();
+    }, []);
     return (
         <AnimationWrapper>
             <section className="h-cover flex justify-center gap-10">
                 {/* latest blogs */}
                 <div className="w-full">
-                    <InPageNavigation routes={["home", "trending blogs"]} defaultHidden={["trending blogs"]}>
-                        <h1>latest blogs</h1>
+                    <InPageNavigation
+                        routes={["home", "trending blogs"]}
+                        defaultHidden={["trending blogs"]}
+                    >
+                        <>
+                        {
+                            blogs == null ? <Loader/> : 
+                            blogs.map((blogs, i)=>{
+                                return <h1 key={i}>{blogs.title}</h1>
+                            })
+                        }
+                        </>
                         <h1>trending blogs</h1>
                     </InPageNavigation>
                 </div>
