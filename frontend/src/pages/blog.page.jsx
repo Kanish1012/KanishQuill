3b612931a5d8908b380095a0c3ed4a86af834fac
Kanish <kanishv1012@gmail.com>
2025-02-19 15:42:56 +0530

    added axios and state management to blog page component

--- frontend/src/pages/blog.page.jsx
+++ frontend/src/pages/blog.page.jsx
@@ -1,11 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-const BlogPage = ()=>{
+export const blogStructure = {
+    title: "",
+    banner: "",
+    author: { personal_info: {} },
+    des: "",
+    content: [],
+    tags: [],
+    publishedAt: "",
+};
 
-    let {blog_id} = useParams()
-    return (
-        <h1>This is a blog page for {blog_id}</h1>
-    )
-}
+const BlogPage = () => {
+    let { blog_id } = useParams();
+    const [blog, setBlog] = useState(blogStructure);
+    let {
+        title,
+        content,
+        banner,
+        author: {
+            personal_info: { fullname, username, profile_img },
+        },
+        publishedAt,
+    } = blog;
+
+    const fetchBlog = () => {
+        axios
+            .post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", { blog_id })
+            .then(({ data: { blog } }) => {
+                setBlog(blog);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    useEffect(() => {
+        fetchBlog();
+    }, []);
+
+    return <h1>This is a blog page for {blog.title}</h1>;
+};
 
 export default BlogPage;
