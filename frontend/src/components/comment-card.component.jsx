6cae7823dfec4b7150c2603d6a817db57ab2c8f3
Kanish <kanishv1012@gmail.com>
2025-03-07 15:48:36 +0530

    Add Load More button for replies and fix reply loading logic

--- frontend/src/components/comment-card.component.jsx
+++ frontend/src/components/comment-card.component.jsx
@@ -112,23 +112,27 @@ const CommentCard = ({ index, leftVal, commentData }) => {
     };
 
     // Load replies for a comment
-    const loadReplies = ({ skip = 0 }) => {
-        if (children.length) {
+    const loadReplies = ({ skip = 0, currentIndex = index }) => {
+        if (commentsArr[currentIndex].children.length) {
             hideReplies();
 
             axios
                 .post(import.meta.env.VITE_SERVER_DOMAIN + "/get-replies", {
-                    _id,
+                    _id: commentsArr[currentIndex]._id,
                     skip,
                 })
                 .then(({ data: { replies } }) => {
-                    commentData.isReplyLoaded = true;
+                    commentsArr[currentIndex].isReplyLoaded = true;
 
                     for (let i = 0; i < replies.length; i++) {
                         replies[i].childrenLevel =
-                            commentData.childrenLevel + 1;
+                            commentsArr[currentIndex].childrenLevel + 1;
 
-                        commentsArr.splice(index + 1 + i + skip, 0, replies[i]);
+                        commentsArr.splice(
+                            currentIndex + 1 + i + skip,
+                            0,
+                            replies[i]
+                        );
                     }
 
                     setBlog({
@@ -178,6 +182,46 @@ const CommentCard = ({ index, leftVal, commentData }) => {
         }
 
         setReplying((preVal) => !preVal);
+    };
+
+    // Load More button for replies
+    const LoadMoreRepliesButton = () => {
+        let parentIndex = getParentIndex();
+        let button = (
+            <button
+                onClick={() =>
+                    loadReplies({
+                        skip: index - parentIndex,
+                        currentIndex: parentIndex,
+                    })
+                }
+                className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2"
+            >
+                Load mroe replies
+            </button>
+        );
+        if (commentsArr[index + 1]) {
+            if (
+                commentsArr[index + 1].childrenLevel <
+                commentsArr[index].childrenLevel
+            ) {
+                if (
+                    index - parentIndex <
+                    commentsArr[parentIndex].children.length
+                ) {
+                    return button;
+                }
+            }
+        } else {
+            if (parentIndex) {
+                if (
+                    index - parentIndex <
+                    commentsArr[parentIndex].children.length
+                ) {
+                    return button;
+                }
+            }
+        }
     };
 
     return (
@@ -258,6 +302,8 @@ const CommentCard = ({ index, leftVal, commentData }) => {
                         ""
                     )}
                 </div>
+
+                <LoadMoreRepliesButton />
             </div>
         </>
     );
