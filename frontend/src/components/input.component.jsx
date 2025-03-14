f83452901deeb79ba03669550f80ac53ac94af5b
Kanish <kanishv1012@gmail.com>
2025-03-14 11:27:43 +0530

    Add InputBox component to Edit Profile page with disabled fields and character limit for bio

--- frontend/src/components/input.component.jsx
+++ frontend/src/components/input.component.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const InputBox = ({ name, type, id, value, placeholder, icon }) => {
+const InputBox = ({ name, type, id, value, placeholder, icon, disable=false }) => {
     const [passwordVisible, setPasswordVisible] = useState(false);
 
     return (
@@ -18,6 +18,7 @@ const InputBox = ({ name, type, id, value, placeholder, icon }) => {
                 placeholder={placeholder}
                 defaultValue={value}
                 id={id}
+                disabled={disable}
                 className="input-box"
             />
             <i className={"fi " + icon + " input-icon"}></i>
--- frontend/src/pages/edit-profile.page.jsx
+++ frontend/src/pages/edit-profile.page.jsx
@@ -5,6 +5,7 @@ import { profileDataStructure } from "./profile.page";
 import AnimationWrapper from "../common/page-animation";
 import Loader from "../components/loader.component";
 import { Toaster } from "react-hot-toast";
+import InputBox from "../components/input.component";
 
 const EditProfile = () => {
     let {
@@ -12,8 +13,22 @@ const EditProfile = () => {
         userAuth: { access_token },
     } = useContext(UserContext);
 
+    let bioLimit = 150;
+
     const [profile, setProfile] = useState(profileDataStructure);
     const [loading, setLoading] = useState(true);
+    const [charactersLeft, setCharactersLeft] = useState(bioLimit);
+
+    let {
+        personal_info: {
+            fullname,
+            username: profile_username,
+            profile_img,
+            email,
+            bio,
+        },
+        social_links,
+    } = profile;
 
     useEffect(() => {
         if (access_token) {
@@ -31,6 +46,10 @@ const EditProfile = () => {
         }
     }, [access_token]);
 
+    const handleCharacterChange = (e) => {
+        setCharactersLeft(bioLimit - e.target.value.length);
+    };
+
     return (
         <AnimationWrapper>
             {loading ? (
@@ -38,6 +57,82 @@ const EditProfile = () => {
             ) : (
                 <form>
                     <Toaster />
+
+                    <h1 className="max-md:hidden">Edit Profile</h1>
+                    <div className="flex flex-col lg:flex-row items-start py-10 gap-8 lg:gap-10">
+                        <div className="max-lg:center mb-5">
+                            <label
+                                htmlFor="uploadImage"
+                                id="profileImgLabel"
+                                className="relative block w-48 h-48 bg-grey rounded-full overflow-hidden"
+                            >
+                                <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center text-white bg-black/30 opacity-0 hover:opacity-100 cursor-pointer">
+                                    Upload Image
+                                </div>
+                                <img src={profile_img} />
+                            </label>
+                            <input
+                                type="file"
+                                id="uploadImage"
+                                accept=".jpeg, .png, .jpg"
+                                hidden
+                            />
+
+                            <button className="btn-light mt-5 max-lg:center lg:w-full px-10">
+                                Upload
+                            </button>
+                        </div>
+
+                        <div className="w-full">
+                            <div className="grid grid-cols-1 md:grid-cols-2 md:gap-5">
+                                <div>
+                                    <InputBox
+                                        name="fullanme"
+                                        type="text"
+                                        value={fullname}
+                                        placeholder="Full name"
+                                        disable={true}
+                                        icon="fi-rr-user"
+                                    />
+                                </div>
+
+                                <div>
+                                    <InputBox
+                                        name="email"
+                                        type="email"
+                                        value={email}
+                                        placeholder="Email"
+                                        disable={true}
+                                        icon="fi-rr-envelope"
+                                    />
+                                </div>
+                            </div>
+
+                            <InputBox
+                                type="text"
+                                name="username"
+                                value={profile_username}
+                                placeholder="Username"
+                                icon="fi-rr-at"
+                            />
+
+                            <p className="text-dark-grey -mt-3">
+                                Username will be used to search user and will be
+                                visible to all users
+                            </p>
+
+                            <textarea
+                                name="bio"
+                                maxLength={bioLimit}
+                                defaultValue={bio}
+                                className="input-box h-64 lg:h-40 resize-none leading-7 mt-5 pl-5"
+                                placeholder="bio"
+                                onChange={handleCharacterChange}
+                            ></textarea>
+
+                            <p className="mt-1 text-dark-grey">{charactersLeft} characters left</p>
+                        </div>
+                    </div>
                 </form>
             )}
         </AnimationWrapper>
